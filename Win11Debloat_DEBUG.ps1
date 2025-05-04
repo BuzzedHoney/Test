38e49b3a139297d987d206d7b0d0dd390c8a6922
bluethedoor <bluethedoor@gmail.com>
2025-05-04 13:53:40 -0500

    Update Win11Debloat_DEBUG.ps1

--- Win11Debloat_DEBUG.ps1
+++ Win11Debloat_DEBUG.ps1
@@ -1,9 +1,6 @@
 $directoryPath = "$env:LOCALAPPDATA\Temp\Win11Debloat\Win11Debloat-master"
 New-Item -ItemType Directory -Force -Path $directoryPath | Out-Null
 
-Set-Content -Path "$directoryPath\CustomAppsList" -Value 
-"
-Microsoft.Edge
-"
+[System.IO.File]::WriteAllText("$directoryPath\CustomAppsList", "Microsoft.Edge")
 
 Start-Process powershell -ArgumentList "-WindowStyle Minimized -Command & ([scriptblock]::Create((Invoke-RestMethod 'https://debloat.raphi.re/'))) -Silent -RemoveAppsCustom -DisableTelemetry -DisableSuggestions -DisableLockscreenTips -DisableWidgets -DisableStartRecommended -ShowHiddenFolders -ShowKnownFileExt -HideSearchTb -DisableFastStartup -DisableStickyKeys"
