8b0bcf2620364428b3128765d59c61a9cdc75aeb
bluethedoor <bluethedoor@gmail.com>
2025-05-04 13:55:53 -0500

    Update Win11Debloat_DEBUG.ps1

--- Win11Debloat_DEBUG.ps1
+++ Win11Debloat_DEBUG.ps1
@@ -1,18 +1,20 @@
 $directoryPath = "$env:LOCALAPPDATA\Temp\Win11Debloat\Win11Debloat-master"
 New-Item -ItemType Directory -Force -Path $directoryPath | Out-Null
 
-[System.IO.File]::WriteAllText("$directoryPath\CustomAppsList", "Clipchamp.Clipchamp
+[System.IO.File]::WriteAllText("$directoryPath\CustomAppsList", "
+Clipchamp.Clipchamp
 Microsoft.3DBuilder
-Microsoft.549981C3F5F10   #Cortana app
+Microsoft.549981C3F5F10
 Microsoft.BingFinance
-Microsoft.BingFoodAndDrink            
-Microsoft.BingHealthAndFitness         
+Microsoft.BingFoodAndDrink
+Microsoft.BingHealthAndFitness
 Microsoft.BingNews
 Microsoft.BingSports
 Microsoft.BingTranslator
-Microsoft.BingTravel 
+Microsoft.BingTravel
 Microsoft.BingWeather
-Microsoft.Getstarted   # Cannot be uninstalled in Windows 11
-Microsoft.Messaging")
+Microsoft.Getstarted
+Microsoft.Messaging
+")
 
 Start-Process powershell -ArgumentList "-WindowStyle Minimized -Command & ([scriptblock]::Create((Invoke-RestMethod 'https://debloat.raphi.re/'))) -Silent -RemoveAppsCustom -DisableTelemetry -DisableSuggestions -DisableLockscreenTips -DisableWidgets -DisableStartRecommended -ShowHiddenFolders -ShowKnownFileExt -HideSearchTb -DisableFastStartup -DisableStickyKeys"
