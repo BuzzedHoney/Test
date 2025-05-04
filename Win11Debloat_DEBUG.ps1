30ce6ecdfa8556c3b48d200cae7c585d8461945b
bluethedoor <bluethedoor@gmail.com>
2025-05-04 16:55:34 -0500

    Update Win11Debloat_DEBUG.ps1

--- Win11Debloat_DEBUG.ps1
+++ Win11Debloat_DEBUG.ps1
@@ -2,4 +2,8 @@ New-Item -ItemType Directory -Force -Path "$env:LOCALAPPDATA\Temp\Win11Debloat\W
 
 Invoke-RestMethod 'https://raw.githubusercontent.com/bluethedoor/Test/main/FinalAppList' | Set-Content "$env:LOCALAPPDATA\Temp\Win11Debloat\Win11Debloat-master\CustomAppsList"
 
-Start-Process powershell -ArgumentList "-WindowStyle Minimized -Command & ([scriptblock]::Create((Invoke-RestMethod 'https://debloat.raphi.re/'))) -Silent -RemoveAppsCustom -DisableTelemetry -DisableSuggestions -DisableLockscreenTips -DisableWidgets -DisableStartRecommended -ShowHiddenFolders -ShowKnownFileExt -HideSearchTb -DisableFastStartup -DisableStickyKeys"
+& ([scriptblock]::Create((irm "https://debloat.raphi.re/"))) -Silent -RemoveAppsCustom -DisableTelemetry -DisableSuggestions -DisableLockscreenTips -DisableDesktopSpotlight -DisableWidgets -ShowHiddenFolders -ShowKnownFileExt -DisableFastStartup -DisableStickyKeys
+
+# Start-Process powershell -ArgumentList "-WindowStyle Minimized -Command & ([scriptblock]::Create((Invoke-RestMethod 'https://debloat.raphi.re/'))) -Custom CMDS"
+
+# -DisableStartRecommended -HideSearchTb
