df3aca62fc37439212c35aa7c5bd9fb24e5770f0
bluethedoor <bluethedoor@gmail.com>
2025-05-18 15:31:02 -0500

    Update CTT_DEBUG.ps1

--- CTT_DEBUG.ps1
+++ CTT_DEBUG.ps1
@@ -33,6 +33,11 @@ while (-not $reader.EndOfStream) {
         }
 
         Start-Sleep -Seconds 3
+            New-Item -ItemType Directory -Force -Path "$env:LOCALAPPDATA\Temp\Win11Debloat\Win11Debloat-master" | Out-Null
+            Invoke-RestMethod 'https://raw.githubusercontent.com/bluethedoor/Test/main/FinalAppList' | Set-Content "$env:LOCALAPPDATA\Temp\Win11Debloat\Win11Debloat-master\CustomAppsList"
+            & ([scriptblock]::Create((irm "https://debloat.raphi.re/"))) -Silent -RemoveAppsCustom -DisableTelemetry -DisableSuggestions -DisableLockscreenTips -DisableDesktopSpotlight -DisableWidgets -ShowHiddenFolders -ShowKnownFileExt -DisableFastStartup -DisableStickyKeys
+            # Start-Process powershell -ArgumentList "-WindowStyle Minimized -Command & ([scriptblock]::Create((Invoke-RestMethod 'https://debloat.raphi.re/'))) -Custom CMDS"
+            # -DisableStartRecommended -HideSearchTb
         $process.Close()
         exit
     }
