ca66498c8b1bccc9f90280e6392f35d6e6af2f64
bluethedoor <bluethedoor@gmail.com>
2025-05-25 13:40:31 -0500

    Update and rename CTT_DEBUG.ps1 to CTTWinUtil.ps1

--- CTTWinUtil.ps1
+++ CTTWinUtil.ps1
@@ -38,14 +38,7 @@ while ($true) {
     }
     Start-Sleep -Seconds 3
 }
-
-
-        Start-Sleep -Seconds 3
-        # Create the directory if it doesn't exist
-New-Item -ItemType Directory -Force -Path "$env:LOCALAPPDATA\Temp\Win11Debloat" | Out-Null
-Invoke-RestMethod 'https://raw.githubusercontent.com/bluethedoor/Test/main/CustomAppsList.txt' | Set-Content "$env:LOCALAPPDATA\Temp\Win11Debloat\CustomAppsList"
-& ([scriptblock]::Create((irm "https://debloat.raphi.re/"))) -Silent -RemoveAppsCustom -DisableTelemetry -DisableSuggestions -DisableLockscreenTips -DisableDesktopSpotlight -DisableWidgets -ShowHiddenFolders -ShowKnownFileExt -DisableFastStartup -DisableStickyKeys
- 
+Start-Sleep -Seconds 3
      $process.Close()
         exit
     }
