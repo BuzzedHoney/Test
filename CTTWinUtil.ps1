5ef2515c5f48124397a17efe69d3c661ed20a396
bluethedoor <bluethedoor@gmail.com>
2025-05-25 13:52:42 -0500

    Update CTTWinUtil.ps1

--- CTTWinUtil.ps1
+++ CTTWinUtil.ps1
@@ -1,3 +1,4 @@
+# Set up the ProcessStartInfo for running PowerShell
 $psi = New-Object System.Diagnostics.ProcessStartInfo
 $psi.FileName = "powershell.exe"
 $psi.Arguments = '-NoProfile -Command "iex \"& { $(irm christitus.com/win) } -Config https://raw.githubusercontent.com/bluethedoor/Test/main/Tweaks.json -Run\""'
@@ -5,13 +6,18 @@ $psi.RedirectStandardOutput = $true
 $psi.UseShellExecute = $false
 $psi.CreateNoWindow = $true
 
+# Start the process and get the standard output reader
 $process = [System.Diagnostics.Process]::Start($psi)
 $reader = $process.StandardOutput
 
+# Read output line by line
 while (-not $reader.EndOfStream) {
     $line = $reader.ReadLine()
-    Write-Output $line      # <--- THIS LINE MAKES OUTPUT VISIBLE IN C#
+    Write-Output $line  # This line makes output visible in C#
+
+    # When the tweaks are finished, close the utility window and handle Disk Cleanup
     if ($line -match "Tweaks are Finished") {
+        # Find and close the Chris Titus Tech utility window
         $apps = Get-Process | Where-Object { $_.MainWindowTitle }
         foreach ($app in $apps) {
             if ($app.MainWindowTitle -like "*Chris Titus Tech's Windows Utility*") {
@@ -20,26 +26,29 @@ while (-not $reader.EndOfStream) {
             }
         }
 
-while ($true) {
-    $diskCleanup = Get-Process | Where-Object { $_.MainWindowTitle -like "*Disk Cleanup*" }
-    $diskNotif = Get-Process | Where-Object { $_.MainWindowTitle -like "*Disk Space Notification*" }
-    # If there are any Disk Cleanup windows still open, keep waiting
-    if ($diskCleanup.Count -gt 0) {
-        Start-Sleep -Seconds 3
-        continue
-    }
-    # If there are no Disk Cleanup windows and at least one notification, proceed
-    if ($diskNotif.Count -gt 0) {
-        Start-Sleep -Seconds 3
-        foreach ($notif in $diskNotif) {
-            Stop-Process -Id $notif.Id -Force
+        # Wait for Disk Cleanup to finish, then close Disk Space Notification windows
+        while ($true) {
+            $diskCleanup = Get-Process | Where-Object { $_.MainWindowTitle -like "*Disk Cleanup*" }
+            $diskNotif = Get-Process | Where-Object { $_.MainWindowTitle -like "*Disk Space Notification*" }
+
+            if ($diskCleanup.Count -gt 0) {
+                Start-Sleep -Seconds 3
+                continue
+            }
+
+            if ($diskNotif.Count -gt 0) {
+                Start-Sleep -Seconds 3
+                foreach ($notif in $diskNotif) {
+                    Stop-Process -Id $notif.Id -Force
+                }
+                break
+            }
+
+            Start-Sleep -Seconds 3
         }
-        break
-    }
-    Start-Sleep -Seconds 3
-}
-Start-Sleep -Seconds 3
-     $process.Close()
+
+        Start-Sleep -Seconds 3
+        $process.Close()
         exit
     }
 }
