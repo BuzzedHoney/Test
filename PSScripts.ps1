231f5ba1ae8c9a3a393d6a17424cbac0e5401756
bluethedoor <bluethedoor@gmail.com>
2025-05-25 13:41:14 -0500

    Update PSScripts.ps1

--- PSScripts.ps1
+++ PSScripts.ps1
@@ -1,4 +1,4 @@
-irm "https://raw.githubusercontent.com/bluethedoor/Test/main/Win11Debloat_DEBUG.ps1" | iex //CTTWINUTIL
+irm "https://raw.githubusercontent.com/bluethedoor/Test/main/CTTWinUtil.ps1" | iex //CTTWINUTIL
 irm "https://raw.githubusercontent.com/bluethedoor/Test/main/Win11Debloat_DEBUG.ps1" | iex //WIN11DEBLOAT
 irm "https://raw.githubusercontent.com/bluethedoor/Test/main/Win11Debloat_DEBUG.ps1" | iex //EDGE, ONEDRIVE, OUTLOOK REMOVER
 irm "https://raw.githubusercontent.com/bluethedoor/Test/main/Win11Debloat_DEBUG.ps1" | iex //OPTIMIZER
