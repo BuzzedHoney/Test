dd17d581fde5bcfca22222b1ea99878d35854f26
Buzzed <bluethedoor@gmail.com>
2025-07-14 01:10:47 -0500

    Update OSDetection.ps1

--- OSDetection.ps1
+++ OSDetection.ps1
@@ -10,11 +10,11 @@ $editionDisplay = switch ($osEdition) {
 }
 
 $tweakSupport = switch -Wildcard ("$osVersion $osEdition") {
-    "*Windows 11* Professional" { "Full tweak support (Windows 11 Pro)" }
-    "*Windows 11* Home"         { "Limited tweak support (Windows 11 Home)" }
-    "*Windows 10* Professional" { "Most tweaks will work (Windows 10 Pro)" }
-    "*Windows 10* Home"         { "Extremely limited tweak support (Windows 10 Home)" }
-    default                     { "Unknown or unsupported Windows edition" }
+    "*Windows 11* Professional" { "w111p" } 
+    "*Windows 11* Home"         { "w11h" } 
+    "*Windows 10* Professional" { "w10p" } 
+    "*Windows 10* Home"         { "w10h" } 
+    default                     { "d" } 
 }
 
 Write-Host "$tweakSupport`n"
