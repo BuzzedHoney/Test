bbffb4462b5c325b1a726e9b19ec698198b1576b
Buzzed <bluethedoor@gmail.com>
2025-07-18 22:56:13 -0500

    Update Windows Defender.ps1

--- Windows Defender.ps1
+++ Windows Defender.ps1
@@ -1,5 +1,7 @@
 Set-MpPreference -DisableRealtimeMonitoring $false
 
+Set-MpPreference -AllowDevRemotePerformanceMode 0
+
 Set-MpPreference -MAPSReporting Advanced
 
 Set-MpPreference -SubmitSamplesConsent 0
