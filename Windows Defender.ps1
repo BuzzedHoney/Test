6e5772e940929f3249954207f54c75c1f224a398
Buzzed <bluethedoor@gmail.com>
2025-07-20 19:45:09 -0500

    Update Windows Defender.ps1

--- Windows Defender.ps1
+++ Windows Defender.ps1
@@ -1,8 +1,16 @@
-Write-Host "Applying Windows Defender Tweaks"
+Write-Host "Applying Security Tweaks"
+
+Write-Host "Configuring Windows Defender"
+
+Write-Host "Updating Windows Defender"
 
 Update-MpSignature
 
+Write-Host "Enabling Windows Defender"
+
 Set-MpPreference -DisableRealtimeMonitoring $false
+
+Write-Host "Configuring Windows Defender"
 
 Set-MpPreference -PerformanceModeStatus Disabled
 
@@ -12,7 +20,12 @@ Set-MpPreference -SubmitSamplesConsent 0
 
 Set-MpPreference -EnableControlledFolderAccess Enabled
 
+Write-Host "Configuring Firewall"
+
+Write-Host "Enabling Firewall"
+
 Set-NetFirewallProfile -Profile Domain,Private,Public -Enabled True
 
-Get-NetConnectionProfile | Where-Object {$_.NetworkCategory -ne 'Public'} | ForEach-Object { Set-NetConnectionProfile -InterfaceIndex $_.InterfaceIndex -NetworkCategory Public }
+Write-Host "Configuring Firewall"
 
+Get-NetConnectionProfile | Where-Object {$_.NetworkCategory -ne 'Public'} | ForEach-Object { Set-NetConnectionProfile -InterfaceIndex $_.InterfaceIndex -NetworkCategory Public }
