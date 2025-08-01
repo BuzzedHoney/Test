7d06124c6d7040c7835e7ba3d3fb99764970a1bb
Buzzed <174978011+BuzzedHoney@users.noreply.github.com>
2025-08-01 09:44:11 -1200

    Update OneDrive Cleanup.ps1

--- OneDrive Cleanup.ps1
+++ OneDrive Cleanup.ps1
@@ -5,6 +5,8 @@ do {
     $cfaStatus = (Get-MpPreference).EnableControlledFolderAccess
 } while ($cfaStatus -ne 0)
 
+Start-Sleep 3
+
 $userProfile = [Environment]::GetFolderPath("UserProfile")
 
 $excludedFolders = @(
@@ -27,11 +29,15 @@ $paths.Values | ForEach-Object {
     }
 }
 
-$folders = Get-ChildItem -Path $userProfile -Directory -Force | Where-Object {
-    $excludedFolders -notcontains $_.Name -and -not ($_.Name.StartsWith('.'))
-}
+$files = @(
+    Get-ChildItem -Path $userProfile -Directory -Force | Where-Object {
+        $excludedFolders -notcontains $_.Name -and -not ($_.Name.StartsWith('.'))
+    }
+) + @(
+    Get-ChildItem -Path $userProfile -File -Force -ErrorAction SilentlyContinue
+) #If you have a better solution for this please let me know because its very slow.
 
-foreach ($folder in $folders) {
+foreach ($folder in $files) {
     Get-ChildItem -Path $folder.FullName -Recurse -File -Force -ErrorAction SilentlyContinue | ForEach-Object {
         $ext = $_.Extension.ToLowerInvariant()
         $destination = $null
@@ -44,6 +50,7 @@ foreach ($folder in $folders) {
             { $_ -in ".exe", ".msi", ".bat" } { $destination = $paths.Apps; break }
         }
         if ($destination) {
+            try {
                 $destPath = Join-Path $destination $_.Name
                 $i = 1
                 while (Test-Path $destPath) {
@@ -53,7 +60,11 @@ foreach ($folder in $folders) {
                     $i++
                 }
                 Move-Item -Path $_.FullName -Destination $destPath -Force
+            } catch {  #If you have a better solution for this please let me know because idk what else to put.
             }
         }
     }
-    Set-MpPreference -EnableControlledFolderAccess Enabled
+}
+
+Set-MpPreference -EnableControlledFolderAccess Enabled
+# This is tied to the CTT WinUtil's OneDrive Tweak which moves items in one drive into the C:\Users\[Username], so I decided to add this to make it auto seperate the files based on their type into desagnated folders.
